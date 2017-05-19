b8d4fb2e89eab84542955ac01d1529f2ff5e3e37
mousa ageeli <mousaageeli@gmail.com>
2017-05-20 02:14:00 +0300

    bavkoeojpkpo

--- www/js/browser.js
+++ www/js/browser.js
@@ -3,7 +3,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     document.addEventListener("backbutton", onBackKeyDown, false);
     function onBackKeyDown(e) {
-        if(window.location('index.html')){
+        if($.mobile.activePage.is('#homepage')){
             if (confirm("Are you sure you want to exit app?")) {
                 e.preventDefault();
                 navigator.app.exitApp();
