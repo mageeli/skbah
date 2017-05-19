6a78c50bdedcdd8ffd6f1096c8620feee3ec72ce
mousa ageeli <mousaageeli@gmail.com>
2017-05-19 05:14:38 +0300

    logo

--- platforms/browser/www/js/browser.js
+++ platforms/browser/www/js/browser.js
@@ -1,1 +1,35 @@
 
+
+document.addEventListener("deviceready", inAppBrowserAPI, false);
+
+function inAppBrowserAPI() {
+    var optionArr = [
+        'location=no',
+        'hidden=yes',
+        'clearcache=yes',
+        'clearsessioncache=yes',
+        'transitionstyle=fliphrizontal',
+        'toolbar=yes',
+        'closebuttoncaption=Exit',
+        'toolbarposition=top',
+        'disallowoverscroll=yes',
+        'enableViewportScale=yes',
+        'mediaPlaybackRequireUserAction=yes',
+        'allowInlineMediaPlayback=yes',
+        'keyboardDisplayRequiresUserAction=no',
+        'suppressesIncrementRendering=yes',
+        'presentationstle=formsheet',
+        'zoom=no',
+        'hardwareback=no',
+        'fullscreen=yes'
+    ]
+
+    var options = optionArr.join();
+
+    if (navigator.connection.type == Connection.NONE) {
+        navigator.notification.alert('An internet connection is required to continue');
+    } else {
+        var browserOptions = window.open('https://skbah.com/','_blank', options);
+        browserOptions.show();
+    }
+}
