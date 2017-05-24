20db4f121b9685f35c8e1f9b8e24f3e6a5553eb3
mousa ageeli <mousaageeli@gmail.com>
2017-05-24 04:29:45 +0300

    add all

--- platforms/browser/www/js/browser.js
+++ platforms/browser/www/js/browser.js
@@ -29,46 +29,83 @@ function inAppBrowserAPI() {
         navigator.notification.alert('An internet connection is required to continue');
     } else {
         var browserOptions = window.open('https://skbah.com/','_blank', options);
-        browserOptions.addEventListener('loadstop', function () {
-            browserOptions.insertCSS(
-                {file: 'stylesheets.css'}
-            );
-            browserOptions.insertCSS(
-                {code: 'body #footer {display: none;}'}
-            );
-            browserOptions.insertCSS(
-                {file: 'stylesheets.css'}
-            );
-            browserOptions.insertCSS(
-                {code: 'body  .main-slider {display: none;}'}
-            );
-            browserOptions.insertCSS(
-                {file: 'stylesheets.css'}
-            );
-            browserOptions.insertCSS(
-                {code: '.common-home header {left: 0;position: fixed !important; right: 0; top: 0px; z-index:80; background-color: #fff; border-bottom: 1px solid #333;}'}
-            );
-            browserOptions.insertCSS(
-                {file: 'stylesheets.css'}
-            );
-            browserOptions.insertCSS(
-                {code: '.homepage-text {margin-top: 100px;}'}
-            );
-            browserOptions.insertCSS(
-                {file: 'stylesheets.css'}
-            );
-            browserOptions.insertCSS(
-                {code: '.content-top-breadcum {height: 200px !important;}'}
-            );
-            browserOptions.insertCSS(
-                {file: 'stylesheets.css'}
-            );
-            browserOptions.insertCSS(
-                {code: 'header {left: 0;position: fixed !important; right: 0; z-index:80; background-color: #fff; border-bottom: 1px solid #333;}'}
-            );
-            browserOptions.show();
-        });
+        browserOptions.addEventListener('loadstart', loadStartCallBack);
+        browserOptions.addEventListener('loadstop', loadStopCallBack);
+        browserOptions.addEventListener('loaderror', loadErrorCallBack);
+        function loadStartCallBack() {
 
+            $('#status-message').text("loading please wait ...");
+
+        }
+
+        function loadStopCallBack() {
+            if (browserOptions != undefined) {
+                browserOptions.insertCSS(
+                    {file: 'stylesheets.css'}
+                );
+                browserOptions.insertCSS(
+                    {code: 'body #footer {display: none;}'}
+                );
+                browserOptions.insertCSS(
+                    {file: 'stylesheets.css'}
+                );
+                browserOptions.insertCSS(
+                    {code: 'body  .main-slider {display: none;}'}
+                );
+                browserOptions.insertCSS(
+                    {file: 'stylesheets.css'}
+                );
+                browserOptions.insertCSS(
+                    {code: '.common-home header {left: 0;position: fixed !important; right: 0; top: 0px; z-index:80; background-color: #fff; border-bottom: 1px solid #333;}'}
+                );
+                browserOptions.insertCSS(
+                    {file: 'stylesheets.css'}
+                );
+                browserOptions.insertCSS(
+                    {code: '.homepage-text {margin-top: 100px;}'}
+                );
+                browserOptions.insertCSS(
+                    {file: 'stylesheets.css'}
+                );
+                browserOptions.insertCSS(
+                    {code: '.content-top-breadcum {height: 200px !important;}'}
+                );
+                browserOptions.insertCSS(
+                    {file: 'stylesheets.css'}
+                );
+                browserOptions.insertCSS(
+                    {code: 'header {left: 0;position: fixed !important; right: 0; z-index:80; background-color: #fff; border-bottom: 1px solid #333;}'}
+                );
+                $('#status-message').text("");
+                browserOptions.show();
+            }
+        };
+        function loadErrorCallBack(params) {
+
+            $('#status-message').text("");
+
+            var scriptErrorMesssage =
+                "alert('Sorry we cannot open that page. Message from the server is : "
+                + params.message + "');"
+
+            browserOptions.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
+
+            browserOptions.close();
+
+            browserOptions = undefined;
+
+        }
+
+        function executeScriptCallBack(params) {
+
+            if (params[0] == null) {
+
+                $('#status-message').text(
+                    "Sorry we couldn't open that page. Message from the server is : '"
+                    + params.message + "'");
+            }
+
+        }
     }
 }
 
