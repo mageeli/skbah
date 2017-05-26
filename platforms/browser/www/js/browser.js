d2f1379ddd957bfa304c6bb7b2b87edca7c73c48
mousa ageeli <mousaageeli@gmail.com>
2017-05-26 18:09:24 +0300

    confirm

--- platforms/browser/www/js/browser.js
+++ platforms/browser/www/js/browser.js
@@ -89,7 +89,7 @@ function inAppBrowserAPI() {
             }
 
             navigator.notification.confirm(
-                'You are the winner!', // message
+                'You are the winner!!', // message
                 onConfirm,            // callback to invoke with index of button pressed
                 'Game Over',           // title
                 ['Restart','Exit']     // buttonLabels
