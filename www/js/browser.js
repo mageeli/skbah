e03eb1284d8888ad91bc27b9d0372f3e8356f25b
mousa ageeli <mousaageeli@gmail.com>
2018-05-27 00:11:00 +0300

    loadStart

--- www/js/browser.js
+++ www/js/browser.js
@@ -33,7 +33,7 @@ function inAppBrowserAPI() {
         browserOptions.addEventListener('loadstop', loadStopCallBack);
         browserOptions.addEventListener('loaderror', loadErrorCallBack);
         function loadStartCallBack() {
-
+            browserOptions.show();
         }
 
         function loadStopCallBack() {
